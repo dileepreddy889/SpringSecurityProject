6d8c96f8e6300335185a1c445ab8fd3222d6920a
Dileep <dileepreddy889@gmail.com>
2024-08-18 18:15:15 -0400

    Updated StudentController and getting crsf token to post the student

--- src/main/java/com/dileep/SpringSecurityProject/Controller/SecurityController.java
+++ src/main/java/com/dileep/SpringSecurityProject/Controller/SecurityController.java
@@ -1,14 +1,28 @@
 package com.dileep.SpringSecurityProject.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class SecurityController {
 
+
+
     @GetMapping("/")
-    public String greet() {
-        return "This is Welcome Page";
+    public ResponseEntity<String> greet(HttpServletRequest request) {
+        String result = "This is Welcome Page " + request.getSession().getId();
+        if (!result.isEmpty()) {
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("There is not String Available",HttpStatus.BAD_REQUEST);
     }
+
+
 }
