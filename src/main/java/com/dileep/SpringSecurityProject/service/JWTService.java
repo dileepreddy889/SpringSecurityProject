134e5eb0d7cf48391d72e5f416e1ba78ef8e46a7
Dileep <dileepreddy889@gmail.com>
2024-10-27 14:20:12 -0400

    JwtToken implementation

--- src/main/java/com/dileep/SpringSecurityProject/service/JWTService.java
+++ src/main/java/com/dileep/SpringSecurityProject/service/JWTService.java
@@ -35,7 +35,7 @@ public class JWTService {
                 .add(claims)
                 .subject(userName)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() * 60 * 60 *30))
+                .expiration(new Date(System.currentTimeMillis() + 60 * 60 *30))
                 .and()
                 .signWith(getKey())
                 .compact();
